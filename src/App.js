776d0837c8fc3d55bd3de9935e7d9a62bc98ab52
Sergio Faustino <smfalcas@gmail.com>
2018-12-28 10:49:43 +0000

    Using fetch from jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import Deck from './Deck';
 import SearchBox from './SearchBox.js'
-import {robots} from './robots';
 import './App.css'
 
 class App extends Component {
 	constructor() {
 		super()
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 		}
+	}
+
+	componentDidMount () {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json() )
+		.then (users => this.setState({robots : users}));
 	}
 
 	onSearchChange = (event) => {
