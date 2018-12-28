8cae35d53d14e0a14278938922a891d135a71bd4
Sergio Faustino <smfalcas@gmail.com>
2018-12-28 11:58:44 +0000

    Updates

---
+++ precache-manifest.c42794a1aa64733b9fdd54575d309025.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RobotGenerator/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a37e572b53ada2c9f324",
+    "url": "/RobotGenerator/static/js/runtime~main.a37e572b.js"
+  },
+  {
+    "revision": "5f28b1f7e1a507d3f622",
+    "url": "/RobotGenerator/static/js/main.5f28b1f7.chunk.js"
+  },
+  {
+    "revision": "d4d0dd5f75038cc80a9b",
+    "url": "/RobotGenerator/static/js/1.d4d0dd5f.chunk.js"
+  },
+  {
+    "revision": "5f28b1f7e1a507d3f622",
+    "url": "/RobotGenerator/static/css/main.926ab1c7.chunk.css"
+  },
+  {
+    "revision": "d4d0dd5f75038cc80a9b",
+    "url": "/RobotGenerator/static/css/1.2af347d5.chunk.css"
+  },
+  {
+    "revision": "9abfa13222410c1c4359d33961d96035",
+    "url": "/RobotGenerator/index.html"
+  }
+];
